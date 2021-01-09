fdd32c14e83adc7935d6ad253faafbbfe9bc6b05
SANKET MHATRE <sanket.mhatre@vit.edu.in>
2021-01-09 20:12:30 +0530

    update

--- django-app/newsfresh/newsfresh/settings.py
+++ django-app/newsfresh/newsfresh/settings.py
@@ -24,17 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['127.0.0.1']
-
-'''
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '../newsfresh/static',
-]
-'''
-
 
 
 # Application definition
